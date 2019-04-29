5f41bf27c2599f6a7738137805d66c1ebc4caf45
sadiev <sadiev@itstep.kz>
2019-04-29 16:59:45 +0600

    Добавьте файлы проекта.

---
+++ 16.03/CardTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._03
+{
+  public enum CardTypes
+  {
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+  }
+}
